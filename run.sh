33bd1f3121517ba3b31f7eb0a3986de0d4e1409b
zhangyz11 <1115954174@qq.com>
2018-03-29 14:40:46 +0800

    spring boot run jar的脚本

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+grep_name=$1
+jar_path=$2
+log_path=$3
+if [ -z "$1" ]
+then
+ echo "---------------------------------------------------------------------------"
+ echo "| For example: ./run.sh Artificial.jar /data/Artificial.jar /data/log.file |"
+ echo "| Artificial.jar -> is jar name                                            |"
+ echo "| /data/Artificial.jar -> is jar path                                      |"
+ echo "| /data/log.file -> is log path                                            |"
+ echo "---------------------------------------------------------------------------"
+else
+PID=$(ps -ef | grep $grep_name | grep -v grep | grep -v /bin/bash | awk '{print $2}')
+#echo $(ps -ef | grep $grep_name | grep -v grep | grep -v /bin/bash )
+if [ -z "$PID" ]
+then
+    nohup java -jar $jar_path  >$log_path 2>&1 &
+    echo "Appliction is already start,pid is $(ps -ef | grep $grep_name | grep -v grep | grep -v /bin/bash | awk '{print $2}')"
+else 
+    echo "Appliction is already stoppend,kill is pid $PID"
+    kill $PID	
+fi
+fi
