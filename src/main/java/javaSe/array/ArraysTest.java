97b10605aec75da50aef8b26a7c0ee1584f9ac8f
zhangyuezong <1115954174@qq.com>
2018-05-01 16:36:00 +0800

    测试代码提交

---
+++ src/main/java/javaSe/array/ArraysTest.java
@@ -0,0 +1,12 @@
+package javaSe.array;
+
+import java.util.Arrays;
+
+public class ArraysTest {
+	public static void main(String[] args) {
+		int[] ints = new int[] { 1, 2, 3 };
+	//	Arrays.fill(ints, 11);
+		String a = Arrays.toString(ints);
+		System.out.println(a);
+	}
+}
